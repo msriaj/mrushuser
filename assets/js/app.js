a57d02b9a4446bb13edb2b2da86874f52d8a34b6
MD-Baki <bakimd77@gmail.com>
2023-06-25 18:38:32 +0600

    sas

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,18 @@ $(document).ready(function () {
     $(".delete").on("click", function () {
         $(".modal").removeClass("activet");
     });
-    $(".submited").on("click", function () {
+
+    $(".add_modal_2").on("click", function () {
         $(".modal_two").addClass("activet");
     });
     $(".delete").on("click", function () {
         $(".modal_two").removeClass("activet");
+    });
+
+    $(".show_modal").on("click", function () {
+        $(".total_modal").addClass("show");
+    });
+    $(".delete").on("click", function () {
+        $(".total_modal").removeClass("show");
     });
 });
